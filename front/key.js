19751cbf054ab6be558738675ecf6b83245c0740
Your Name <you@example.com>
2025-01-16 02:10:40 +0600

    Your commit message here

---
+++ front/key.js
@@ -0,0 +1,14 @@
+window.inn = async function (e) {
+     
+
+    console.log(e);
+    if(e=="a^2"){
+        
+    }
+    
+    // let currentLatex = mathField.latex();
+//     mathField.latex(`${currentLatex}^2`);
+
+// mathField.focus();
+
+}
